fe391d484f1341f2e615bf57cbcc8f4d6829e5f7
Larisa <pronina.larisa2003@gmail.com>
2023-04-16 13:06:43 +0300

    Можно 4-5 баллов, пожаалуйста (^ ω ^)

---
+++ lab_06/table.h
@@ -0,0 +1,11 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+#include "structurs.h"
+#include <QTableWidget>
+
+int add_point(const point &p, const colors col, const bool &is_hole, QTableWidget *table, content &data);
+void rewrite_table(const content &data, QTableWidget *table);
+void del_row(const indexes &ind_data, content &data);
+
+#endif // TABLE_H
